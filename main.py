963b378bd5337a4042c49d1e615c3bafd1ec8f99
Martin Sundhaug <martinsundhaug@gmail.com>
2017-03-17 18:12:20 +0100

    Use MYSQL-database instead of local storage

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from scapy.all import *
 from xml.sax.saxutils import escape
+import mysql.connector
 
-assoc_list = []
-nodes = []
-edge_count = 1
+def register_edge(station_mac, ssid):
+    cur.execute('SELECT 1 FROM vifi.edges WHERE station_mac="{}" AND ssid="{}"'.format(station_mac, ssid))
+    r=cur.fetchall()
+    if r == []:
+        cur.execute('INSERT INTO vifi.edges VALUES({},{},"{}","{}")'.format(time.time(),time.time(),station_mac, ssid))
+    else:
+        cur.execute('UPDATE vifi.edges SET last_seen={} WHERE station_mac="{}" AND ssid="{}"'.format(time.time(),station_mac,ssid))
+    cnx.commit()
 
 def PacketHandler(pkt):
-    global assoc_list, nodes, edge_count
     if pkt.haslayer(Dot11):
         if pkt.type == 0 and pkt.subtype == 4 and pkt.info != '':
             probed_ssid = escape(pkt.info)
             probing_mac = escape(pkt.addr2)
             if probed_ssid != pkt.info:
                 return #Hack
-            if probed_ssid not in nodes:
-                nodes.append(probed_ssid)
-            if probing_mac not in nodes:
-                nodes.append(probing_mac)
-            if (probing_mac, probed_ssid) not in assoc_list:
-                assoc_list.append((probing_mac, probed_ssid))
-                edge_count += 1
+            register_edge(probing_mac, probed_ssid)
+
+config = {
+    'user': 'root',
+    'password': 'eviltwin',
+    'host': '127.0.0.1'
+}
+cnx = mysql.connector.connect(**config)
+cur = cnx.cursor()
+
+cur.execute('CREATE DATABASE IF NOT EXISTS vifi;')
+cur.execute('CREATE TABLE IF NOT EXISTS vifi.edges(first_seen DOUBLE, last_seen DOUBLE, station_mac TEXT, ssid TEXT);')
+cnx.commit()
 
 sniff(iface="rename7", prn = PacketHandler)
