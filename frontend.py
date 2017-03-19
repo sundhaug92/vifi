d7afadf54f6be704b72a46a24fc0ca38b35a4487
Martin Sundhaug <martinsundhaug@gmail.com>
2017-03-19 04:46:07 +0100

    frontend.py: Fix MySQL-timeout

--- frontend.py
+++ frontend.py
@@ -4,6 +4,21 @@ import re
 
 app = Flask(__name__)
 
+def sql_execute(query, error_level = 0):
+    config = {
+        'user': 'root',
+        'password': 'eviltwin',
+        'host': '127.0.0.1'
+    }
+    cnx = mysql.connector.connect(**config)
+    cur = cnx.cursor()
+
+    cur.execute(query)
+    return_value = cur.fetchall()
+    cur.close()
+    cnx.close()
+    return return_value
+
 def get_document(ssid=None, mac=None):
     document = ''
     document += '<table><th><b>mac</b></th><th><b>ssid</b></th>\n'
@@ -14,11 +29,10 @@ def get_document(ssid=None, mac=None):
             query += ' AND station_mac="{}"'.format(mac)
     elif not mac is None:
         query += ' WHERE station_mac="{}"'.format(mac)
-    cur.execute(query)
     macs = []
     ssids = []
     edge_count = 0
-    for edge in cur.fetchall():
+    for edge in sql_execute(query):
         document += '<tr><td><a href="/show_mac/{}">{}</a></td><td><a href="/show_ssid/{}">{}</a></td></tr>\n'.format(edge[0], edge[0], edge[1], edge[1])
         macs.append(edge[0])
         ssids.append(edge[1])
@@ -41,10 +55,8 @@ def show_ssid(ssid):
     return get_document(ssid=ssid)
 
 def get_nodes():
-    cur.execute('SELECT DISTINCT station_mac FROM vifi.edges')
-    nodes = cur.fetchall()
-    cur.execute('SELECT DISTINCT ssid FROM vifi.edges')
-    nodes += cur.fetchall()
+    nodes = sql_execute('SELECT DISTINCT station_mac FROM vifi.edges') + \
+            sql_execute('SELECT DISTINCT ssid FROM vifi.edges')
     nodes = list(set(nodes))
     return [n[0].encode('ascii', 'ignore') for n in nodes]
 
@@ -54,7 +66,7 @@ def api_nodes():
     document = 'var nodes = ['
     for node_id in range(len(nodes)):
         is_phone = re.match(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$', nodes[node_id]) != None
-        document += '{id: %s, label: "%s", shape: "image", image: "%s" },' % (node_id, nodes[node_id].encode('ascii', 'ignore'), '\\'+
+        document += '{id: %s, label: "%s", shape: "image", image: "%s" },' % (node_id, nodes[node_id].encode('ascii', 'ignore'), 
         {True:'Hardware-My-PDA-02-icon.png', False:'Network-Pipe-icon.png'}[is_phone])
     document = document[:-1] + '];'
     return document
@@ -63,8 +75,7 @@ def api_nodes():
 def api_edges():
     nodes = get_nodes()
     document = 'var edges = ['
-    cur.execute('SELECT station_mac, ssid FROM vifi.edges')
-    edges = cur.fetchall()
+    edges = sql_execute('SELECT station_mac, ssid FROM vifi.edges')
     for (mac, ssid) in edges:
         mac_node_id = str(nodes.index(mac.encode('ascii', 'ignore')))
         ssid_node_id = str(nodes.index(ssid.encode('ascii', 'ignore')))
@@ -75,14 +86,6 @@ def api_edges():
 @app.route("/static/<path:path>")
 def send_static(path):
     flask.send_from_directory(path)
-
-config = {
-    'user': 'root',
-    'password': 'eviltwin',
-    'host': '127.0.0.1'
-}
-cnx = mysql.connector.connect(**config)
-cur = cnx.cursor()
 
 if __name__ == "__main__":
     app.run()
