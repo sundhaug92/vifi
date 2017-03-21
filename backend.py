5561be82f29959ec9e56709d1cc11f2997259f8b
Martin Sundhaug <martinsundhaug@gmail.com>
2017-03-21 16:45:17 +0100

    backend.py: UTF-8 filtering for SSIDs and anti-MAC-randomization-measures

--- backend.py
+++ backend.py
@@ -5,6 +5,10 @@ import time
 
 def register_edge(station_mac, ssid):
     global cnx, cur
+    if escape(ssid) != ssid or '\x00' in ssid or len(ssid) > 32:
+        return
+    if any([ord(c) not in range(128) for c in ssid]):
+        return
     print(station_mac, ssid)
     config = {
         'user': 'root',
@@ -19,6 +23,10 @@ def register_edge(station_mac, ssid):
         cur.execute('INSERT INTO vifi.edges VALUES(%s,%s,%s,%s)', (time.time(),time.time(),station_mac, ssid))
     else:
         cur.execute('UPDATE vifi.edges SET last_seen=%s WHERE station_mac=%s AND ssid=%s', (time.time(),station_mac,ssid))
+
+
+    cur.execute('DELETE FROM vifi.edges WHERE last_seen<{} AND (station_mac LIKE "_2%" OR station_mac LIKE "_6%" OR station_mac LIKE "_A%" OR station_mac LIKE "_E%")'.format(time.time() - 300)) #Remove private MACs not seen in 5m
+#    cur.execute('DELETE FROM vifi.edges WHERE last_seen<{}'.format(time.time() - (8 * 3600))) # Delete public MACs not seen in 8h
     cnx.commit()
     cnx.close()
 
@@ -28,8 +36,6 @@ def PacketHandler(pkt):
             if pkt.type == 0 and pkt.subtype == 4 and pkt.info != '':
                 probed_ssid = escape(pkt.info)
                 probing_mac = escape(pkt.addr2)
-                if probed_ssid != pkt.info:
-                    return #Hack
                 register_edge(probing_mac, probed_ssid)
         except AttributeError:
             pass
