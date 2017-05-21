15a2841e2b42f0499f568774bf3cf7b70368d007
Martin Sundhaug <martinsundhaug@gmail.com>
2017-05-21 06:53:25 +0200

    Reworked start.sh to fix common startup-issues

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-killall flask
+interface=${1:-"wlan0"}
+
+ps aux|grep flask|grep -v grep > /dev/null && killall flask
+
+if [[ ! $(ifconfig $interface|grep UP) ]]
+then
+    sudo -s -- 'iwconfig $interface mode monitor && ifconfig wlan0 up'
+fi
+
 FLASK_APP=frontend.py flask run --host 0.0.0.0 &
-sudo python backend.py
+sudo python backend.py $interface
